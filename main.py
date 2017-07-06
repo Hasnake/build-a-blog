d29527e8fa71957ff4b83dde8567d22ce7f36909
Haile Asnake <hasnake@sdg-komo-mac014.local>
2017-07-05 20:34:12 -0700

    Adding the request method

--- main.py
+++ main.py
@@ -8,10 +8,12 @@ app.config['SQLALCHEMY_ECHO'] = True
 
 db = SQLAlchemy(app)
 
+
 class Blog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120))
     body = db.Column(db.String(120))
+
 
 def get_blogList():
     return Blog.query.all()
@@ -24,9 +26,26 @@ def newpost_page():
     if request.method == "POST":
         # Save our post
         # TODO: Put save logic here
-
+        blog_title = request.form['blog_Title']
+        blog_content = request.form['blog_NewEntry']
+        newpost = Blog(blog_title, blog_content)
+        db.session.add(newpost)
+        db.session.commit()
+        return redirect("/")
         # TODO: Redirect to create post page if there are errors
         # (Use query parameters)
+    else:
+        error_title=""
+        error_content=""
+        if len(blog_title) >= 120 or len(blog_title)==0:
+            error_title = "Limit message under 120 characthers"
+            blog_title="" #displays empty title instead of the the error content
+        if len(blog_content) >=1000 or len(blog_content)==0:
+            error_content = "Limit message under 1000 characthers"
+            blog_content=""
+        if len(error_title)>0 or len(error_content)>0:
+            posts = Blog.query.all()
+            return render_template('index.html',page_name="Build A Blog!", posts=posts, title="Blog title", content="Blog content")
 
         return redirect("/")
 
@@ -43,31 +62,3 @@ def index():
 
 if __name__ == "__main__":
     app.run()
-
-
-
-
-
-#@app.route("/register", methods=['GET', 'POST'])
-#def register():
-    #if request.method == 'POST':
-        #email = request.form['email']
-        #password = request.form['password']
-        #verify = request.form['verify']
-        #if not is_email(email):
-            #flash('zoiks! "' + email + '" does not seem like an email address')
-            #return redirect('/register')
-        #email_db_count = User.query.filter_by(email=email).count()
-        #if email_db_count > 0:
-            #flash('yikes! "' + email + '" is already taken and password reminders are not implemented')
-            #return redirect('/register')
-        #if password != verify:
-            #flash('passwords did not match')
-            #return redirect('/register')
-        #user = User(email=email, password=password)
-        #db.session.add(user)
-        #db.session.commit()
-        #session['user'] = user.email
-        #return redirect("/")
-    #else:
-        #return render_template('register.html')
